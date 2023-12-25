a5bcc913c03bd4311741df0884acbce2bb2de11d
Patric Stout <truebrain@openttd.org>
2023-12-26 00:21:50 +0100

    Feature: initial version of the Social Presence Plugin for Discord

---
+++ cmake/PackageBundle.cmake
@@ -0,0 +1,28 @@
+string(TIMESTAMP CURRENT_YEAR "%Y")
+
+set(CPACK_BUNDLE_NAME "OpenTTD-${PROJECT_NAME}")
+set(CPACK_BUNDLE_ICON "${CMAKE_SOURCE_DIR}/os/macos/openttd.icns")
+set(CPACK_BUNDLE_PLIST "${CMAKE_CURRENT_BINARY_DIR}/Info.plist")
+set(CPACK_DMG_BACKGROUND_IMAGE "${CMAKE_SOURCE_DIR}/os/macos/splash.png")
+set(CPACK_DMG_FORMAT "UDBZ")
+set(CPACK_DMG_DISABLE_APPLICATIONS_SYMLINK ON)
+set(CPACK_APPLE_BUNDLE_ID "org.openttd.${PROJECT_NAME}")
+
+# Create a temporary Info.plist.in, where we will fill in the version via
+# CPackProperties.cmake.in. This because at this point in time the version
+# is not yet known.
+configure_file("${CMAKE_SOURCE_DIR}/os/macos/Info.plist.in" "${CMAKE_CURRENT_BINARY_DIR}/Info.plist.in")
+set(CPACK_BUNDLE_PLIST_SOURCE "${CMAKE_CURRENT_BINARY_DIR}/Info.plist.in")
+
+get_target_property(LIBRARIES ${PROJECT_NAME} LINK_LIBRARIES)
+string(REGEX REPLACE "([^;]+)" "/Contents/Resources/\\1.dylib" LIBRARIES "${LIBRARIES}")
+set(CPACK_BUNDLE_APPLE_CODESIGN_FILES "/Contents/Resources/lib${PROJECT_NAME}.dylib;${LIBRARIES}")
+
+# Copy "Install.txt" to the bundle root.
+install(
+    CODE
+    "
+        file(COPY \"${CMAKE_SOURCE_DIR}/os/macos/Install.txt\" DESTINATION \${CMAKE_INSTALL_PREFIX}/../../../)
+    "
+    DESTINATION .
+    COMPONENT Runtime)
